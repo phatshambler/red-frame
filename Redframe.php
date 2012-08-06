5c598c23dd9d38a6be9c0edbc68c5dc6a068dea6
Nicolas Roy <phatshambler@gmail.com>
2012-08-06 01:37:31 -0400

    First commit! v0.1

---
+++ Redframe.php
@@ -0,0 +1,278 @@
+<?php
+
+/*
+ *
+ * Red Frame
+ *
+ * 
+ * Simple redis model abstraction class - in dev. Do NOT use for production. Learning redis project.
+ * Requires a Predis\Client instance (Redis Client for PHP) in the constructor. Thank you nrk!
+ * See: https://github.com/nrk/predis
+ *
+ * @author Nicolas Roy <nr@commun.ca>
+ * @version 0.1
+ *
+ *
+ */
+
+abstract class RedFrame {
+	
+	/*
+	 *
+	 * Override these two - see samples folder for sample on how to configure your model
+	 *
+	 */
+
+	protected $superStructure = array(
+			'name' => 'names',
+			'separator' => ':',
+			'type' => 'set', //must be set for now
+			'id' => 'numeric' //for v0.2
+	);
+
+	protected $infraStructure = array(
+
+		'title' => array(
+			'type' => 'string'
+		),
+		/*...*/
+	);
+	
+	
+	/*
+	 *
+	 * Basic stuff
+	 *
+	 */
+	
+	//The Redis Client Object
+	protected $redis;
+
+	//Name of the master key
+	protected $name;
+
+	//Item separator
+	protected $separator;
+	
+	public function __construct($redis = null) {
+		
+		if(isset($redis)) {
+			$this->redis = $redis;
+		}else{
+			return false;
+		}
+
+		$this->superStructure['next'] = $this->infraStructure;
+
+		$this->name = $this->superStructure['name'];
+		$this->separator = $this->superStructure['separator'];
+
+	}
+
+	/*
+	 *
+	 * Main getters
+	 *
+	 */
+
+	public function getRedis(){
+		return $this->redis;
+	}
+
+	public function getItem($id){
+
+		try{
+			$items = array();
+
+			if($this->redis->exists($this->name.$this->separator.$id)){
+				$path = $this->name.$this->separator.$id;
+				
+				foreach($this->infraStructure as $k => $i){
+					$items[$k] = $this->recursiveGet($path.$this->separator.$k, $i);
+				}
+			}
+
+			if(count($items) > 0){
+				return $items;
+			}
+			else{
+				return false;
+			}
+
+		}catch(Exception $e){
+			return false;
+		}
+
+	}
+
+	public function getAllItems(){
+
+		try{
+			$items = array();
+
+			if($this->redis->exists($this->name) && $this->redis->type($this->name) == 'set'){
+				$set = $this->redis->smembers($this->name);
+
+				foreach($set as $s){
+					$items[] = $this->redis->getItem($s);
+				}
+			}
+
+			if(count($items) > 0){
+				return $items;
+			}else{
+				return false;
+			}
+
+		}catch (Exception $e){
+			return false;
+		}
+	}
+
+	protected function recursiveGet($path, $value){
+
+		if(isset($value['next']) && is_array($value['next']) && !empty($value['next'])){
+			$items = array();
+			foreach($value['next'] as $k => $i){
+				$items[$k] = $this->recursiveGet($path.$this->separator.$k, $i);
+			}
+			return $items;
+			
+		}else{
+			return $this->getItemByType($path, $value['type']);
+		}
+
+	}
+
+
+	public function getItemByType($path, $type){
+
+		try{
+
+			if($type == 'set'){
+				return $this->redis->smembers($path);
+			}
+			else if($type == 'sortedset'){
+				return $this->redis->zrange($path, 0, -1);
+			}
+			else if($type == 'string'){
+				return $this->redis->get($path);
+			}
+			else if($type == 'hash'){
+				return $this->redis->hgetall($path);
+			}
+			else if($type == 'list'){
+				return $this->redis->lrange($path, 0, -1);
+			}
+			else{
+				return false;
+			}
+
+		}catch(Exception $e){
+			return false;
+		}
+
+	}
+
+
+	/*
+	 *
+	 * Main setters
+	 *
+	 */
+
+	public function setItem($id, $value){
+
+		try{
+
+			//superStructure type must be 'set' for this to work with getAllItems()
+			$this->setValueByType($this->name, $this->superStructure['type'], $id);
+
+			if(is_array($value)){
+				$this->recursiveSet($this->infraStructure, $value, $this->name.$this->separator.$id);
+				//So the main key for the item exists, i.e. keys:01
+				$this->redis->set($this->name.$this->separator.$id, 1);
+			}
+
+			return true;
+
+		}catch(Exception $e){
+			return false;
+		}
+	}
+
+	protected function recursiveSet($modelArray, $array, $path){
+
+		foreach($array as $k => $v){
+
+			if(array_key_exists($k, $modelArray)){
+
+				if(isset($modelArray[$k]['next']) && is_array($modelArray[$k]['next']) && is_array($v) && !empty($v)){
+					$this->recursiveSet($modelArray[$k]['next'], $v, $path.$this->separator.$k);
+				}
+				else{
+					if(isset($modelArray[$k]['params']) && is_array($modelArray[$k]['params'])){
+						$this->setValueByType($path.$this->separator.$k, $modelArray[$k]['type'], $v, $modelArray[$k]['params']);
+					}else{
+						$this->setValueByType($path.$this->separator.$k, $modelArray[$k]['type'], $v, null);
+					}
+				}
+			}
+		}
+
+	}
+
+	public function setValueByType($key, $type, $value, $params = null){
+
+		try{
+
+			if($type == 'set'){
+				//can be array or not
+				$this->redis->sadd($key, $value);
+			}
+			else if($type == 'sortedset'){
+				//if multiple values for sorted set, scores = scores array, $value = value array
+				if(is_array($value)){
+					
+					foreach($value['scores'] as $k => $v){
+						$this->redis->zadd($key, $v, $value['values'][$k]);
+					}
+					
+				}else{
+					//default, score = 1
+					$this->redis->zadd($key, 1, $value);
+				}
+			}
+			else if($type == 'string'){
+				$this->redis->set($key, $value);
+			}
+			else if($type == 'hash'){
+				//filters the hash members with the provided list
+				//value _must_ be associative array
+				if(isset($params)){
+					if(isset($params['members']) && is_array($params['members'])){
+						foreach($value as $k => $v){
+							if(!in_array($k, $params['members'])) unset($value[$k]);
+						}
+					}
+				}
+				$this->redis->hmset($key, $value);
+			}
+			else if($type == 'list'){
+				//value car be array or item
+				if(isset($params)){
+					if(isset($params['autodel']) && $params['autodel']){
+						$this->redis->del($key);
+					}
+				}
+				$this->redis->rpush($key, $value);
+			}
+
+			return true;
+
+		}catch(Exception $e){
+			return false;
+		}
+
+	}
+}
---
+++ sample_article.php
@@ -0,0 +1,50 @@
+<?php
+
+//Sample class for RedFrame
+
+class Article extends RedFrame {
+
+	public $superStructure = array(
+			'name' => 'articles', //name of the master key, ex. items, articles, animals...
+			'separator' => ':', //item separator
+			'type' => 'set', //To be able to retrieve the set of all the added items
+			'id' => 'numeric'
+	);
+
+	public $infraStructure = array(
+
+		'title' => array(
+			'type' => 'string'
+		),
+		'meta' => array(
+			'type' => 'set',
+			'next' => array( //next acts like a hash but can contain any other type of data
+				'author' => array(
+					'type' => 'string'
+				),
+				'dateadded' => array(
+					'type' => 'string'
+				),
+				'datemodified' => array(
+					'type' => 'list'
+				)
+			)
+		),
+		'tags' => array(
+			'type' => 'set'
+		),
+		'hashed_thing' => array(
+			'type' => 'hash',
+			'params' => array('members' => array('name', 'profession', 'friend')), //list of the allowed members of the hash
+		),
+		'sorted_thing' => array(
+			'type' => 'sortedset' //two arrays _needed_, of the same length, the scores and their associated values
+		),
+		'list_thing' => array(
+			'type' => 'list',
+			'params' => array('autodel' => true) //a list. flushes itself if autodel is true
+		)
+
+	);
+
+}
